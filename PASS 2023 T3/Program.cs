ff313caf3e910bbb5a7c2c5447b5e23ac1eb8504
branrile09 <93716194+branrile09@users.noreply.github.com>
2023-09-29 10:06:38 +0930

    Add project files.

---
+++ PASS 2023 T3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PASS_2023_T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Riley Says Hello");
+            Console.WriteLine("Hi, I'm Peter");
+            Console.WriteLine("Hi, I am Anmol");
+            Console.WriteLine("hi Im David");
+            Console.WriteLine("Hello Taner");
+            Console.WriteLine("Hello, nisarg here");
+            Console.WriteLine("Hi there, Titus");
+
+
+
+        }
+
+    }
+
+}
