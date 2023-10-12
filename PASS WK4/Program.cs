bed9e6b486ecfcf023f014c2116bc2b0b539159d
branrile09 <93716194+branrile09@users.noreply.github.com>
2023-10-12 22:35:02 +1030

    new week, new project
    yea...

---
+++ PASS WK4/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace PASS_WK4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ISE PASS wk4");
+
+            /*TODO LIST:
+             *
+             *  ----- 5min intro/setup -----
+             *
+             *  recap kahoot (variables and basic loops)
+             *
+             *  ----- 25min to go through this -----
+             *
+             *  Loop Types (10min)
+             *  Arrays (10min)
+             *  Lists (5min)
+             *  
+             *  ----- 5 min Break -----
+             *
+             *  Go make a coffee and get some biscuits maybe??
+             *
+             *  ----- 25min to go through this -----
+             *
+             *  Switch (5min)
+             *  Enums   (5min)
+             *  
+             *  Nested Loops (10-15min)
+             * 
+             *  ----- 5-10 min Q&A -----
+             * 
+             * ask anything
+             *               
+             *  ----- Form/Survey(anon) -----
+             *  
+             *  https://forms.office.com/r/Y30Gf5ZDVF
+             *  
+             *  ----- Next week -----
+             *  
+             *  Functions/Methods
+             *      Explanation
+             *      Structure
+             *      Building
+             *      
+             * 
+            */
+
+            //first things first, everyone say Hello and share an unpopular opinion. 
+            Console.WriteLine("RB was here, DISCOS BACK BABY!!!!");
+
+
+
+
+            // ----- Loops ----
+            //1. while loop
+            //a) explanation:            
+            //b) example code (loop while meaning of life is != 42 )
+
+
+            //2. Do While
+            //a) explanation
+            //b) example code (loop while meaning of life is != 42 )
+
+            //3. For loop
+            //a) explanation
+            //b) example code (100 iterations, number starts at 1, triple it, then half it)
+
+            //4, Foreach
+            //a) explanation
+            //b) example code (iterate through each char in "HiPpoPlatyroo", then output how many vowels in the word  )
+
+
+
+            // ----- Arrays ----
+            //1. Create an Array of intigers            
+
+
+            //2. Create an Array of strings
+
+
+            //3. Create a 2d Array of intigers size 20x20
+
+
+            //4. Create a list of boolean
+
+
+            //5. explain the difference between list and array
+            //
+
+
+
+            // ----- Nested Loops ----
+            //1.
+            // from the 2d array from earlier, assign random values between 1 and 100
+
+            //example code for random
+            Random rnd = new Random();
+            int month = rnd.Next(1, 13); // creates a number between 1 and 12
+            int dice = rnd.Next(1, 7); // creates a number between 1 and 6
+            int card = rnd.Next(52); // creates a number between 0 and 51
+
+
+            //
+            //2.
+            //from the 2d array from earlier, find the average.
+
+
+            //using multidensional arrays can be used for all sorts of things, such as AI, Data Science, Game matrix tables, Path finding, etc
+
+
+
+
+
+        }
+    }
+}
---
+++ PASS WK4/temp cs.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PASS_WK4
+{
+    enum Months
+    {
+        January,    // 0
+        February,   // 1
+        March,      // 2
+        April,      // 3
+        May,        // 4
+        June,       // 5
+        July        // 6
+    }
+    enum intys 
+    {
+        January = 1,    // 0
+        February,   // 1
+        March,      // 2
+        April,      // 3
+        May,        // 4
+        June,       // 5
+        July        // 6
+    }
+
+
+
+    internal class temp_cs
+    {
+        temp_cs() { }
+
+        
+        void randomshiz()
+        {
+
+            int temp = 1;
+            string bluh = "";
+
+            switch (temp)
+            {
+                case 1:
+                    bluh = "BLUH!";
+                    break;
+
+                case 2:
+                    bluh = "bluh1";
+                    break;
+            }
+
+            bool isAlphanum = true;
+            bool hasDigit = false;
+            bool hasLetter = false;
+            foreach (char c in bluh)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    isAlphanum = false;
+                    break;
+                }
+                else
+                {
+
+                    if (char.IsLetter(c) && !hasLetter)
+                    {
+                        hasLetter = true;
+                    }
+                    else if (char.IsDigit(c) && !hasDigit)
+                    {
+                        hasDigit = true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    if (hasDigit && hasLetter)
+                    { break; }
+                }
+
+            }
+
+            if (!isAlphanum) { Console.WriteLine("False"); }
+            else { Console.WriteLine("True"); }
+
+        }
+
+    }
+}
