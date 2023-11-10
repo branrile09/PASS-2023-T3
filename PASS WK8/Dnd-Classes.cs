b577bbace330543869b78dc6057dd44648eaab1a
branrile09 <93716194+branrile09@users.noreply.github.com>
2023-11-10 10:43:43 +1030

    Wk8 Inheritence
    Dnd theme??

---
+++ PASS WK8/Dnd-Classes.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PASS_WK8
+{
+    //as much as i dislike structs, this is super useful
+    public struct Stats
+    {
+        public int strength = 10;
+        public int dexterity = 10;
+        public int constituion = 10;
+        public int intelligence = 10;
+        public int wisdom = 10;
+        public int charisma = 10;
+
+        public Stats(int str, int dex, int consti, int intel, int wis, int charisma)
+        {
+            strength = str;
+            dexterity = dex;
+            constituion = consti;
+            intelligence = intel;
+            wisdom = wis;
+            this.charisma = charisma;
+        }
+
+    }
+
+    /// <summary>
+    /// this is our base class
+    /// </summary>
+    internal class Player
+    {
+        //core stats
+        protected float health = 100;
+        protected string name = "";
+        protected int level = 1;
+        protected float exp = 0;
+
+        //ability stats
+        protected int strength = 10;
+        protected int dexterity = 10;
+        protected int constituion = 10;
+        protected int intelligence = 10;
+        protected int wisdom = 10;
+        protected int charisma = 10;
+
+        //combat moves (4)
+        protected string CombatMove1 = "";
+        protected string CombatMove2 = "";
+        protected string CombatMove3 = "";
+        protected string CombatMove4 = "";
+
+
+        public Player()
+        {
+
+        }
+
+        public Player(string name, Stats newStats)
+        {
+            this.name = name;
+            AssignStats(newStats);
+        }
+
+        /// <summary>
+        /// instead of doing this for each constructor, we can reuse this code
+        /// </summary>
+        /// <param name="newStats"></param>
+        protected void AssignStats(Stats newStats)
+        {
+            strength = newStats.strength;
+            dexterity = newStats.dexterity;
+            constituion = newStats.constituion;
+            intelligence = newStats.intelligence;
+            wisdom = newStats.wisdom;
+            charisma = newStats.charisma;
+        }
+
+        protected void gainExp(float xp)
+        {
+            exp += xp;
+            if (xp >= 30 * level)
+            {
+                level++;
+                xp = 0;
+                Console.WriteLine($"{name}: is now level{level}");
+            }
+
+        }
+
+        public float DealDamage()
+        {
+            return combatChoice();
+        }
+
+        protected float combatChoice()
+        {
+            Console.WriteLine($"combat choises are as follows");
+            Console.WriteLine($"1:{CombatMove1} 2:{CombatMove2} 3:{CombatMove3} 4:{CombatMove4}");
+            int choice = int.Parse(Console.ReadLine());
+            return callCombatMethod(choice);
+        }
+
+        //this needs to be re-written for each class
+        protected virtual float callCombatMethod(int i)
+        {
+            float damage = 0;
+            switch (i)
+            {
+                case 1:
+                    damage = 1;
+                    Console.WriteLine($"{name}: used {CombatMove1} for {damage}Damage");
+                    break;
+                case 2:
+                    damage = 2;
+                    Console.WriteLine($"{name}: used {CombatMove2} for {damage}Damage");
+                    break;
+                case 3:
+                    damage = 3;
+                    Console.WriteLine($"{name}: used {CombatMove3} for {damage}Damage");
+                    break;
+                case 4:
+                    damage = 4;
+                    Console.WriteLine($"{name}: used {CombatMove4} for {damage}Damage");
+                    break;
+            }
+            gainExp(damage);
+            return damage;
+        }
+
+        public void TakeDamage(float d)
+        {
+            health -= d;
+        }
+
+        public bool isAlive()
+        {
+            return health > 0;
+        }
+
+    }
+
+    internal class Warrior : Player
+    {
+        public Warrior(string name, Stats newStats)
+        {
+            this.name = name;
+            AssignStats(newStats);
+            CombatMove1 = "Punch";
+            CombatMove2 = "Kick";
+            CombatMove3 = "Bash";
+            CombatMove4 = "Stab";
+        }
+
+
+        protected override float callCombatMethod(int i)
+        {
+            return base.callCombatMethod(i);
+
+        }
+
+    }
+
+    internal class Mage : Player
+    {
+        public Mage(string name, Stats newStats)
+        {
+            this.name = name;
+            AssignStats(newStats);
+            CombatMove1 = "Fire Blast";
+            CombatMove2 = "Teleport";
+            CombatMove3 = "Silence";
+            CombatMove4 = "Polymorph";
+        }
+
+        protected override float callCombatMethod(int i)
+        {
+            return base.callCombatMethod(i);
+
+        }
+
+    }
+
+
+    internal class Archer : Player
+    {
+        public Archer(string name, Stats newStats)
+        {
+            this.name = name;
+            AssignStats(newStats);
+            CombatMove1 = "Head shot";
+            CombatMove2 = "Sniper";
+            CombatMove2 = "Deadly Aim";
+            CombatMove4 = "Cloack";
+        }
+
+        protected override float callCombatMethod(int i)
+        {
+            return 1;
+
+        }
+
+        public void specialAbility()
+        {
+
+
+        }
+
+    }
+}
---
+++ PASS WK8/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace PASS_WK8
+{
+    /*TODO LIST:
+            *
+            *  ----- 5min intro/setup -----
+            *
+            *  Hellos, plan for todays workshop,
+            *  everyone write a message in the hello world function               
+            *
+            *  ----- 25min to go through this -----
+            *
+            *  1.refresh on what objects and inheritence
+            *  2.lets make some DnD Sub-Classes 
+            *  
+            *  ----- 5 min Break -----
+            *
+            *  Go make a coffee and get some biscuits maybe??
+            *
+            *  ----- 25min to go through this -----
+            *  
+            *  3. 
+            *  4. 
+            *  
+            *  ----- 5-10 min Q&A -----
+            * 
+            *   ask anything
+            *               
+            *  ----- Form/Survey(anon) -----         
+            *  
+            *  Mid term feedback form
+            *
+            *  ----- Additional Resources -----
+            *
+            *  //i will add some here when i find some
+            *  
+            *  ----- Next week -----
+            *  
+            *  revise custom objects (Module 6)
+            *  Objects and inheritance (Module 7)
+            *      
+            * 
+           */
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "RB";
+            Stats newStat = new Stats(10,10,10,10,10,10);
+            Warrior newWarrior = new Warrior(name, newStat);
+
+            name = "MG";
+            newStat = new Stats(8,8,8,8,8,8);
+            Mage newMage = new Mage(name, newStat);
+
+
+            do
+            {
+                //damage phase
+                float W_DealDamage = newWarrior.DealDamage();
+                float M_DealDamage = newMage.DealDamage();
+
+
+                newWarrior.TakeDamage(M_DealDamage);
+                newMage.TakeDamage(W_DealDamage);
+
+            } while (newMage.isAlive() && newWarrior.isAlive());
+
+
+            Archer newArcher = new Archer(name, newStat);
+
+            newArcher.specialAbility();
+
+            List<Player> players = new List<Player>();
+            players.Add(newArcher);
+            players.Add(newMage);
+            players.Add(newWarrior);
+
+
+            players[0].DealDamage();
+
+
+            if (players[0].GetType() == typeof(Archer))
+            {
+                Archer tempArcher = (Archer)players[0];
+                tempArcher.specialAbility();
+            }
+
+
+
+        }
+    }
+}
